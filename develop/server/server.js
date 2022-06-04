21bec4c5cf81faddbc9a289ab0fd8bc054010119
Samuel Mandel <slman.760@gmail.com>
2022-06-03 17:28:48 -0700

    develop folder added, server.js switched to Apollo server from restful api server, schema folder and files created

---
+++ develop/server/server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+//apollo Server for graphql
+const {ApolloServer} = require('apollo-server-express')
+const path = require('path');
+//require schemas, typeDefs & resolvers for Graphql
+const {typeDefs, resolvers} = require('./schemas')
+const db = require('./config/connection');
+// routes for restful API
+//const routes = require('./routes');
+const app = express();
+const PORT = process.env.PORT || 3001;
+//server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+//extended from true (restful) to false (graphql)
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+//same as restful
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+//app no longer uses routes here
+//app.use(routes);
+
+//create a new instance of an Apollo server using graphql schema
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  //apply middleware
+  server.applyMiddleware({app})
+
+  db.once('open', () => {
+    app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+    //graphql path
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+}
+startApolloServer(typeDefs, resolvers);
